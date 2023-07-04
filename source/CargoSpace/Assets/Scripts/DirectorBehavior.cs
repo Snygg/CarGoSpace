53ab8f15431e0ea7859d7800c90cfb70782b3694
zero <squiredd@gmail.com>
2023-07-04 15:07:10 -0700

    fire laser

--- source/CargoSpace/Assets/Scripts/DirectorBehavior.cs
+++ source/CargoSpace/Assets/Scripts/DirectorBehavior.cs
@@ -1,17 +1,25 @@
 using Bus;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Logging;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DirectorBehavior : BusParticipant
 {
     public GameObject DummyNpc;
     public GameObject DirectorObject;
+    private List<GameObject> _npcs = new List<GameObject>();
+    public GameObject PlayerShipObject;
+    [FormerlySerializedAs("LazerPrefab")] public GameObject LaserPrefab;
+    private LogBehavior _logger;
 
     // Start is called before the first frame update
     void Start()
     {
+        _logger = LogManager.Initialize(LogObject);
         AddLifeTimeSubscription(Subscribe("npcCreate", OnCreateNpc));
         AddLifeTimeSubscription(Subscribe("npcCommand", OnNpcCommand));
     }
@@ -32,10 +40,25 @@ public class DirectorBehavior : BusParticipant
         var go = Instantiate<GameObject>(DummyNpc, location, new Quaternion());
         var follow = go.GetComponent<NpcFollowPlayerBehavior>();
         follow.DirectorObject = DirectorObject;
+        _npcs.Add(go);
     }
 
     private async Task OnNpcCommand(Dictionary<string, string> body)
     {
-        
+        if (body == null)
+        {
+            _logger.Bus.LogError("body was null", context:this);
+            return;
+        }
+
+        if (!body.ContainsKey("command"))
+        {
+            return;
+        }
+        _logger.Combat.LogDebug("Got command {0}", values: body);
+        var go = Instantiate<GameObject>(LaserPrefab, Vector3.zero, new Quaternion());
+        var lineRenderer = go.GetComponentInChildren<LineRenderer>();
+        lineRenderer.positionCount=2;
+        lineRenderer.SetPositions(new []{Vector3.zero, PlayerShipObject.transform.position});
     }
 }
---
+++ source/CargoSpace/Assets/Scripts/Weapons/SelfDestructFrameCountBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructFrameCountBehavior : MonoBehaviour
+{
+    public int FramesRemaining = 0;
+
+    public GameObject ToDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FramesRemaining > 0)
+        {
+            FramesRemaining--;
+            return;
+        }
+
+        if (ToDestroy)
+        {
+            Destroy(ToDestroy);    
+        }
+    }
+}
