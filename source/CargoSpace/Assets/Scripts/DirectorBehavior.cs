253d78fef032310ed616e19637185eed83303aaf
Snygg <rachel.snygg@gmail.com>
2023-07-07 21:26:11 -0700

    Create behavior to auto fire at a selected target from a module designated as a turret

--- source/CargoSpace/Assets/Scripts/DirectorBehavior.cs
+++ source/CargoSpace/Assets/Scripts/DirectorBehavior.cs
@@ -10,13 +10,14 @@ using UnityEngine.Serialization;
 
 public class DirectorBehavior : BusParticipant
 {
+    private List<GameObject> _npcs = new List<GameObject>();
+    private LogBehavior _logger;
+
     public GameObject DummyNpc;
     public GameObject DirectorObject;
-    private List<GameObject> _npcs = new List<GameObject>();
+    public GameObject PlayerTargeted { get; private set; }
     public GameObject PlayerShipObject;
     public GameObject LaserPrefab;
-    private LogBehavior _logger;
-    private GameObject _playerTargeted;
 
     // Start is called before the first frame update
     void Start()
@@ -24,51 +25,109 @@ public class DirectorBehavior : BusParticipant
         _logger = LogManager.Initialize(LogObject);
         AddLifeTimeSubscription(Subscribe("npcCreate", OnCreateNpc));
         AddLifeTimeSubscription(Subscribe("npcCommand", OnNpcCommand));
-        AddLifeTimeSubscription(Subscribe("PlayerFired",OnPlayerFired));
+        AddLifeTimeSubscription(Subscribe("playerClicked", OnPlayerClicked));
+        AddLifeTimeSubscription(Subscribe("turretFired", OnTurretFired));
     }
 
-    private async Task OnPlayerFired(Dictionary<string, string> arg)
+    private async Task OnPlayerClicked(Dictionary<string, string> body)
     {
-        if (arg == null)
+        const string key = "location";
+        if (!body.ContainsKey(key))
         {
-            _logger.Bus.LogError(new ArgumentException("arg was null", nameof(arg)));
+            _logger.Bus.LogError(new ArgumentException($"arg does not contain {key}", nameof(body)));
             return;
         }
 
-        const string positionKey = "Position";
-        if (!arg.ContainsKey(positionKey))
+        if (!VectorUtils.TryParseVector2(body[key], out var location))
         {
-            _logger.Bus.LogError(new ArgumentException($"arg does not contain {positionKey}", nameof(arg)));
+            _logger.Bus.LogError(new ArgumentException($"{key} not a valid vector", nameof(body)));
             return;
         }
-        
-        if(!VectorUtils.TryParseVector2(arg[positionKey], out var targetPosition))
+
+        var clicked = GetClickedObject(GetRayCastHitList(location));
+        if (_npcs.Contains(clicked)) // next: needs to only set target if turret is selected
         {
-            _logger.Bus.LogError(new ArgumentException($"{positionKey} not a valid vector", nameof(arg)));
-            return;
+            PlayerTargeted = clicked;
+            //publish 
+            Publish("playerTargetSelected", new Dictionary<string, string>
+            {
+                { "hasTarget", "true" }
+            });
         }
-        
-        //todo: pass in list, so we know all the things we hit
-        var playerPosition = PlayerShipObject.transform.position;
-        var targetDirection = targetPosition - (Vector2)playerPosition;
+        //else if (clicked == <one of the turrets>)
+        //{
+        //embellish to add turrets, check if turret is selected, then go into targeting mode
+        //}
+        else
+        {
+            PlayerTargeted = null;
+            Publish("playerTargetSelected", new Dictionary<string, string>
+            {
+                { "hasTarget", "false" }
+            });
+        }
+    }
+
+    private List<RaycastHit2D> GetRayCastHitList(Vector2 clickLocation)
+    {
         List<RaycastHit2D> hitResults = new List<RaycastHit2D>();
-        Physics2D.Raycast(playerPosition, targetDirection, new ContactFilter2D(),hitResults);
-        var raycastHit2D = hitResults.FirstOrDefault(hr=>
-            hr.collider && 
+        Physics2D.Raycast(clickLocation, Vector2.up * .00001f, new ContactFilter2D(), hitResults);
+        return hitResults;
+    }
+
+    private GameObject GetClickedObject (List<RaycastHit2D> hitResults)
+    {
+        var result = hitResults.FirstOrDefault(hr =>
+            hr.collider &&
+            hr.collider.gameObject &&
+            hr.collider.gameObject.GetComponent<TargetableBehavior>());
+
+        if (!result)
+        {
+            return null;
+        }
+
+        return result.collider.gameObject;
+    }
+
+    private async Task OnTurretFired(Dictionary<string, string> body)
+    {
+        const string key = "source";
+        if (!body.ContainsKey(key))
+        {
+            _logger.Bus.LogError(new ArgumentException($"arg does not contain {key}", nameof(body)));
+            return;
+        }
+
+        if (!VectorUtils.TryParseVector2(body[key], out var location))
+        {
+            _logger.Bus.LogError(new ArgumentException($"{key} not a valid vector", nameof(body)));
+            return;
+        }
+
+
+        FireLaser(location, (Vector2) PlayerTargeted.transform.position - location);
+    }
+
+
+
+    private void FireLaser(Vector2 source, Vector2 targetDirection)
+    {
+        List<RaycastHit2D> hitResults = new List<RaycastHit2D>();
+        Physics2D.Raycast(source, targetDirection, new ContactFilter2D(), hitResults);
+        var raycastHit2D = hitResults.FirstOrDefault(hr =>
+            hr.collider &&
             hr.collider.gameObject &&
             hr.collider.gameObject.GetComponent<TargetableBehavior>());
         if (raycastHit2D)
         {
             var gameObject = raycastHit2D.collider.gameObject;
-            //Debug.DrawLine(playerPosition,gameObject.transform.position, Color.red,1);
-            FireLazer(playerPosition,gameObject.transform.position);
-            _playerTargeted = gameObject;
+            RenderLazer(source, gameObject.transform.position);
             _logger.Combat.LogDebug("Player hit target");
         }
         else
         {
-            _playerTargeted = null;
-            FireLazer(playerPosition,targetPosition * 100);
+            RenderLazer(source, targetDirection * 100);
             _logger.Combat.LogVerbose("Player fired and missed");
         }
     }
@@ -112,12 +171,12 @@ public class DirectorBehavior : BusParticipant
             var startPosition = npc.transform.position;
             var endPosition = PlayerShipObject.transform.position;
             
-            FireLazer(startPosition, endPosition);
+            RenderLazer(startPosition, endPosition);
         }
         
     }
 
-    private void FireLazer(Vector3 startPosition, Vector3 endPosition)
+    private void RenderLazer(Vector3 startPosition, Vector3 endPosition)
     {
         var go = Instantiate<GameObject>(LaserPrefab, Vector3.zero, new Quaternion());
         var lineRenderer = go.GetComponentInChildren<LineRenderer>();
