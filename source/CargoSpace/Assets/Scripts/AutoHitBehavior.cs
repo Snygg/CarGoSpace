7ce1340321c650678212a09fe5a7d5468a0bd472
Snygg <rachel.snygg@gmail.com>
2023-07-07 21:41:12 -0700

    Create behavior to auto fire at a selected target from a module designated as a turret (#15)

--- source/CargoSpace/Assets/Scripts/AutoHitBehavior.cs
+++ source/CargoSpace/Assets/Scripts/AutoHitBehavior.cs
@@ -1,29 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Bus;
 using UnityEngine;
 
 public class AutoHitBehavior : BusParticipant
 {
+    private DateTime? _nextShot;
+
+    public double Interval;
+    public GameObject Turret;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddLifeTimeSubscription(Subscribe("playerTargetSelected", OnPlayerTargetSelected));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_nextShot == null)
+        {
+            return;
+        }
+        if (_nextShot <= DateTime.Now)
+        {
+            Fire(Turret.transform.position, "fake", 9000.1f);
+            SetNextShot();
+        }
     }
 
-    private void Fire(string target, string source, string type, float strength)
+    private void SetNextShot() => _nextShot = DateTime.Now.AddSeconds(Interval);
+
+    private async Task OnPlayerTargetSelected(Dictionary<string, string> body)
     {
-        var body = new Dictionary<string, string>();
-        body.Add("target", target);
-        body.Add("source", source);
-        body.Add("strength", strength.ToString());
-        body.Add("type", type);
-        Publish($"AutoHit{target}", body);
+        //check if has target
+        if (!body.TryGetValue("hasTarget", out var hasTarget))
+        {
+            //log
+            return;
+        }
+        if (!bool.TryParse(hasTarget, out var has) || !has)
+        {
+            _nextShot = null;
+            return;
+        }
+
+        SetNextShot();
+    }
+
+    private void Fire(Vector2 source, string type, float strength)
+    {
+        Publish("turretFired", new Dictionary<string, string>
+        {
+            { "source", source.ToString() },
+            { "type", type },
+            { "strength", strength.ToString() }
+        });
     }
 }
